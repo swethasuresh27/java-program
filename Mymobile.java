b4dd65aca991236292efa046263b92124bdee950
swethasuresh27 <104843567+swethasuresh27@users.noreply.github.com>
2022-05-12 10:57:25 +0530

    Add files via upload

---
+++ Mymobile.java
@@ -0,0 +1,14 @@
+package mobile;
+
+import homework.Mobile;
+
+public class Mymobile {
+	
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Mobile mob=new Mobile();
+	mob.call();
+
+	}
+
+}
---
+++ OddorEven.java
@@ -0,0 +1,16 @@
+package homework;
+
+public class OddorEven {
+
+                public void great() {
+				for(int i=1;i<=20;i++) {
+					if(i%2==1)
+						System.out.println(i);
+				}
+			}
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+							OddorEven obj=new OddorEven();
+				obj.great();
+			}
+}
